0190e2500ebc44238843b0c2e7a9de60662cb4f8
1988fsc <msn.box@outlook.com>
2024-01-03 10:43:07 -0300

    update array
    update array

---
+++ Logica.cs
@@ -0,0 +1,199 @@
+﻿                //Casting - TryParse
+    /*string a = "25f";
+int.TryParse(a, out int b);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine("Conversão realizada");*/
+
+                //Operadores Condicionais
+
+    //if else - Confirma quantidade estoque
+    /*int quantidadeEmEstoque = 2;
+    int quantidadeCompra = 0;
+    bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= 
+    quantidadeCompra;
+
+    Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+    Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+    Console.WriteLine($"Possível Venda? {possivelVenda}");
+    if (quantidadeCompra == 0)
+    {
+        Console.WriteLine ("Venda inválida");
+    }
+    else if(possivelVenda)
+    {
+        Console.WriteLine($"Venda realizada.");
+    }
+    else
+    {
+        Console.WriteLine($"Desculpe, estoque insuficiente.");
+    }*/
+
+    //switch case - Verificar se é vogal
+    /*Console.WriteLine("Digite uma Letra");
+    string letra = Console.ReadLine();
+
+    switch(letra)
+    {
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+            Console.WriteLine ("Vogal");
+            break;
+        default:
+            Console.WriteLine ("Não é uma vogal");
+            break;
+    }*/
+
+    //Or - true||false
+    /*bool maiorIdade = false;
+    bool autorizadoMenor = false;
+
+    if(maiorIdade || autorizadoMenor)
+    {
+        Console.WriteLine("liberado");
+    }
+    else
+    {
+        Console.WriteLine("Não liberado");
+    }*/
+
+    //And - &&
+    /*bool presenca = true;
+    double media = 9;
+
+    if(presenca && media>= 7)
+    {
+        Console.WriteLine("Aprovado");
+    }
+    else
+    {
+        Console.WriteLine("Reprovado");
+    }*/
+
+    //Not
+    /*bool choveu = false;
+            bool estaTarde = false;
+
+            if (!choveu && !estaTarde)
+            {
+                Console.WriteLine("Vou Pedalar");
+            }
+            else
+            {
+                Console.WriteLine("Pedalar outro dia");
+            }*/
+
+
+                //Class Math
+
+    //Pow
+    /*public void Potencia (int x, int y)
+        {
+            double pot = Math.Pow(x,y);
+            Console.WriteLine($"{x}^{y} = {pot}");
+        }*/
+
+    // Sin Cos Tan
+    /*public void Seno (double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo} graus = {Math.Round(seno,4)}");
+        }
+        public void Coseno (double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Coseno de {angulo} graus = {Math.Round(coseno,4)}");
+        }
+        public void Tangente (double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Seno de {angulo} graus = {Math.Round(tangente,4)}");
+        }*/
+
+    // Square root
+    /*public void RaizQuadrada(double x)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine($"Raiz de {x} = {raiz}");
+            }*/
+
+                //Laço de repetição
+
+    //For
+    /*int numero = 5;
+
+    for(int contador = 1; contador <=10; contador++)
+    {
+    Console.WriteLine($"{numero}x{contador} = {numero * contador}");
+    }*/
+
+    //While
+    /*int numero =5;
+    int contador = 1;
+
+    while(contador <=10)
+    {
+    Console.WriteLine($"{contador } Execução: {numero} x {contador} = {numero * contador}");
+    contador++;
+    }*/
+
+    // do while - Soma números
+    /*int soma = 0, numero = 0;
+    do
+    {
+    Console.WriteLine("Digite números para somar e 0 para sair");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    soma += numero;
+
+    }while(numero != 0);
+    Console.WriteLine($"Total da somma é :{soma}");*/
+
+    //While - Menu
+    /*string opcao;
+        bool exibirMenu = true;
+        while(exibirMenu)
+        {
+        Console.WriteLine("Digite a opção:");
+        Console.WriteLine("1 -> Cadastro");
+        Console.WriteLine("2 -> Pesquisa");
+        Console.WriteLine("3 -> Apagar registro");
+        Console.WriteLine("4 -> Encerrar");
+
+        opcao = Console.ReadLine();
+
+        switch (opcao)
+        {
+        case "1":
+        Console.Clear();
+        Console.WriteLine("Cadastro");
+        break;
+        case "2":
+        Console.Clear();
+        Console.WriteLine("Pesquisa");
+        break;
+        case "3":
+        Console.Clear();
+        Console.WriteLine("Apagar registro");
+        break;
+        case "4":
+        Console.Clear();
+        Console.WriteLine("Encerrar");
+        exibirMenu = false;
+        break;
+
+        default:
+        Console.WriteLine("Opção inválida");
+        break;
+        }
+
+        }
+
+        Console.WriteLine("Finalizando Aplicação");*/
+
