6dabbd029e0dabbcb646b7c4c095b512c6f1df2c
1988fsc <msn.box@outlook.com>
2024-01-03 12:38:20 -0300

    end

---
+++ Library/Array.cs
@@ -0,0 +1,38 @@
+﻿//Array
+/*int[] arrayInteiros = new int[4];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+arrayInteiros[3] = 1;
+
+Console.WriteLine("Percorrendo o Array com o For");
+for(int contador = 0; contador < arrayInteiros.Length; contador++)
+{
+    Console.WriteLine($"Posição {contador} - {arrayInteiros[contador]}");
+}
+
+Console.WriteLine ("Percorrendo o Array com o Foreach");
+
+int contadorForeach = 0;
+foreach(int valor in arrayInteiros)
+{
+    Console.WriteLine($"Posição {contadorForeach} - {valor}");
+    contadorForeach++;
+}*/
+
+//List
+
+/*List<string> listaString = new List<string>();
+
+listaString.Add("SP");
+listaString.Add("SC");
+listaString.Add("MG");
+listaString.Add("BH");
+listaString.Add("RS");
+
+Console.WriteLine($"Itens na lista: {listaString.Count} - Capacidade atual: {listaString.Capacity}");
+listaString.Add("PB");
+Console.WriteLine($"Itens na lista: {listaString.Count} - Capacidade atual: {listaString.Capacity}");
+listaString.Remove("PB");
+Console.WriteLine($"Itens na lista: {listaString.Count} - Capacidade atual: {listaString.Capacity}");*/
